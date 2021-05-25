d91ab089e5393210b4a740db6ab09c92116f92cc
qiaoguoqiang <qiaoguoqiang@wsecar.com>
2021-05-25 20:39:00 +0800

    netty group chat

--- springmvc/netty-learning/src/main/java/com/yang/netty/bytebuf/ByteBufTest.java
+++ springmvc/netty-learning/src/main/java/com/yang/netty/bytebuf/ByteBufTest.java
@@ -2,6 +2,7 @@ package com.yang.netty.bytebuf;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.util.CharsetUtil;
 import org.junit.Test;
 
 public class ByteBufTest {
@@ -26,5 +27,38 @@ public class ByteBufTest {
         }
 
         System.out.println("执行完毕");
+    }
+
+    @Test
+    public void test02() {
+        // 1. 创建ByteBuf
+        ByteBuf byteBuf = Unpooled.copiedBuffer("hello world!", CharsetUtil.UTF_8);
+
+        // 2. 使用相关方法
+        if (byteBuf.hasArray()) {
+            byte[] content = byteBuf.array();
+
+            // 将content转换成字符串
+            System.out.println(new String(content, CharsetUtil.UTF_8));
+            System.out.println("byteBuf = " + byteBuf);
+
+            System.out.println(byteBuf.arrayOffset());
+            System.out.println(byteBuf.readerIndex());
+            System.out.println(byteBuf.writerIndex());
+            System.out.println(byteBuf.capacity());
+
+            System.out.println(byteBuf.getByte(0));
+
+            int len = byteBuf.readableBytes();
+            System.out.println("len = " + len);
+
+            // 使用for取出各个字节
+            for (int i = 0; i < len; i++) {
+                System.out.println((char) byteBuf.getByte(i));
+            }
+
+            System.out.println(byteBuf.getCharSequence(0, 4, CharsetUtil.UTF_8));
+            System.out.println(byteBuf.getCharSequence(4, 6, CharsetUtil.UTF_8));
+        }
     }
 }
---
+++ springmvc/netty-learning/src/main/java/com/yang/netty/chat/GroupChatServer.java
@@ -0,0 +1,59 @@
+package com.yang.netty.chat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class GroupChatServer {
+    /**
+     * 监听端口
+     */
+    private int port;
+
+    public GroupChatServer(int port) {
+        this.port = port;
+    }
+
+    /**
+     * 编写run方法，处理客户端请求
+     * @throws Exception
+     */
+    public void run() throws Exception {
+        /**
+         * 1. 创建两个线程池
+         */
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast("decoder", new StringDecoder());
+                            pipeline.addLast("encoder", new StringEncoder());
+                            pipeline.addLast(new GroupChatServerHandler());
+                        }
+                    });
+            System.out.println("netty 服务器启动");
+            ChannelFuture channelFuture = b.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new GroupChatServer(7000).run();
+    }
+}
